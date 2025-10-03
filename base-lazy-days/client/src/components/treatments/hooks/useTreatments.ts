fc412e1778be957fe050e764bc78678e9db6ff12
Erick Gamez <erick.gamez@3pillarglobal.com>
2025-10-03 12:48:15 -0600

    lecture 39

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -7,14 +7,14 @@ import { queryKeys } from "@/react-query/constants";
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
-  const { data } = await axiosInstance.get('/treatments');
+  const { data } = await axiosInstance.get("/treatments");
   return data;
 }
 
 export function useTreatments(): Treatment[] {  
-  const { data } = useQuery({
+  const { data = [] } = useQuery({
     queryKey: [queryKeys.treatments],
-    queryFn: getTreatments
-  })
+    queryFn: getTreatments,
+  });
   return data;
 }
