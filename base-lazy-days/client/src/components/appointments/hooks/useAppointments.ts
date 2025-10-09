a6eed26559d2baa0dec5d67e9229b4afb46259ff
Erick Gamez <erick.gamez@3pillarglobal.com>
2025-10-09 12:02:20 -0600

    lecture 48

--- base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import dayjs from "dayjs";
 import { useState } from "react";
 
@@ -61,7 +62,11 @@ export function useAppointments() {
   //
   //    2. The getAppointments query function needs monthYear.year and
   //       monthYear.month
-  const appointments: AppointmentDateMap = {};
+  // const appointments: AppointmentDateMap = {};
+  const { data: appointments = [] } = useQuery({
+    queryKey: [queryKeys.appointments],
+    queryFn: () => getAppointments(monthYear.year, monthYear.month)
+  }) 
 
   /** ****************** END 3: useQuery  ******************************* */
 
